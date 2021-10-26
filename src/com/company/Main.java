6ffe83833f578a1d75a7c272775d2b4e3ca62f58
Miles Johnson <mgj2@cecs.pdx.edu>
2021-10-26 14:42:36 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+//Miles Johnson 2021
+//CS 350
+//Portland State University
+
+//This program takes in "text" input and a "pattern" as command line arguments.
+//Both arguments are treated as strings.  A brute-force algorithm for matching
+//the pattern to the text is executed.  The program prints a visualization of
+//the text compared to the pattern on each iteration of the algorithm, then
+//returns the index of the first element in the substring of the text that
+//matches the pattern.
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+    //stringMatch iterates through the text, comparing each element to the
+    //pattern.  When a character in the text does not match the coinciding
+    //character in the pattern, the starting index of comparison increments.
+    //If the starting index of comparison is greater than the difference of
+    //the length of the text and the length of the pattern, the algorithm
+    //returns unsuccessful.  If the entirety of the pattern matches a
+    //substring of the text, the program reports a success and returns the
+    //starting index of the matching substring in the text.
+    public static int stringMatch(String text, String pattern){
+        for (int i = 0; i < n; i++){
+
+        }
+    }
+
+    //visualize prints a formatted representation of the text with the
+    //pattern below, lined up with the current indices of comparison.
+    public void visualize(int index, String text, String pattern){
+
+    }
+
+    //Ensures that the pattern is not larger than the text
+    public boolean testForSize()
+}
