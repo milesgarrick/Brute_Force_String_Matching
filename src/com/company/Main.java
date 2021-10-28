fe7638c737b04a073b59337b0c7941015f755294
Miles Johnson <mgj2@cecs.pdx.edu>
2021-10-28 14:43:27 -0700

    Consolidated some code into functions, added parsing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,44 +10,60 @@
 //matches the pattern.
 
 package com.company;
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.lang.StringBuilder;
+import java.util.concurrent.ThreadLocalRandom;
 //import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
         //Code block adapted from https://www.baeldung.com/java-random-string for generating a random string
-        int leftLimit = 41;
-        int rightLimit = 122;
-        int targetStringLength = 10000;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
+        int counter = 0;
+        long sum = 0;
+        do {
+            int leftLimit = 41;
+            int rightLimit = 122;
+            int targetStringLength = 20000;
+            Random random = new Random();
+            int patternLength = ThreadLocalRandom.current().nextInt(2, 5);
+            StringBuilder buffer = new StringBuilder(targetStringLength);
 
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String text = buffer.toString();
-        //End of code from https://www.baeldung.com/java-random-string
+            for (int i = 0; i < targetStringLength; i++) {
+                int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            String text = buffer.toString();
+            buffer.setLength(0);
+            for (int i = 0; i < patternLength; i++) {
+                int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            String pattern = buffer.toString();
+            System.out.println(text.length());
+            System.out.println(pattern.length());
+            //End of code from https://www.baeldung.com/java-random-string
 
-        //String text = args[0];              //Records text input
-        String pattern = "FA";
-        //String pattern = args[1];           //Records pattern to match
-        long startTime = System.nanoTime();
-        int matchIndex = stringMatch(text, pattern);
-        long endTime = System.nanoTime();
-        if(matchIndex >= 0) {               //If there is a match
+            //String text = args[0];              //Records text input
+            //String pattern = args[1];           //Records pattern to match
+            long elapsedTime = stringMatch(text, pattern);
+            sum += elapsedTime;
+            ++counter;
+        } while (counter <= 9);
+        System.out.print("Average time: ");
+        System.out.print(sum/10);
+        System.out.println(" microseconds");
+        /*
+        if(elapsedTime >= 0) {               //If there is a match
             System.out.print("\nThe index of the matching substring is ");
-            System.out.println(matchIndex);
+            System.out.println(elapsedTime);
         }
         else{                               //If there is no match
             System.out.println("\nNo match to pattern ");
             System.out.println(pattern);
         }
-        System.out.print("\nElapsed time: ");
-        System.out.print((endTime - startTime)/1000000);
-        System.out.println(" ms");
+         */
     }
 
     //stringMatch iterates through the text, comparing each element to the
@@ -58,7 +74,7 @@ public class Main {
     //returns unsuccessful.  If the entirety of the pattern matches a
     //substring of the text, the program reports a success and returns the
     //starting index of the matching substring in the text.
-    public static int stringMatch(String text, String pattern){
+    public static long stringMatch(String text, String pattern){
         if(text.length() < pattern.length()){
             System.out.println("Input length must be longer than pattern.");
             return -1;
@@ -68,6 +84,7 @@ public class Main {
         int max = textLength - patternLength;
 
         //System.out.println(text);
+        long startTime = System.nanoTime();
         for (int i = 0; i <= max; i++){
             //visualize(i, pattern);
             int j = 0;
@@ -75,14 +92,37 @@ public class Main {
                 ++j;
             }
             if (j == patternLength) {
-                System.out.println("==================");
+                /*System.out.println("==================");
                 System.out.println("Final Comparison:");
                 System.out.println(text);
                 visualize(i, pattern);
-                return i;
+                System.out.print("\nElapsed time: ");
+                System.out.print((endTime - startTime));//1000000);
+                System.out.println(" ms");
+                 */
+                long endTime = System.nanoTime();
+                /*
+                System.out.print("\nElapsed time: ");
+                System.out.print((endTime - startTime)/1000);
+                System.out.println(" microseconds");
+                */
+                return (endTime - startTime)/1000;
+            }
+            else{
+                /*
+                System.out.print("\nElapsed time: ");
+                System.out.print((endTime - startTime));//1000);
+                System.out.println(" ns");
+                 */
             }
         }
-        return -1;
+        long endTime = System.nanoTime();
+        /*
+        System.out.print("\nElapsed time: ");
+        System.out.print((endTime - startTime)/1000);
+        System.out.println(" microseconds");
+         */
+        return (endTime - startTime)/1000;
     }
 
     //visualize prints a formatted representation of the text with the
